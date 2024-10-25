76cf18a32ba417421210e0a21f331b17391ea429
Pichsinee Nanreakmeechai <mild@viewp.local>
2024-10-25 12:49:10 +0700

    first commit

---
+++ MauiCarRace/classes/car.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MauiCarRace.classes;
+
+public class Car
+{
+    public string Name { get; set; }
+    public double Speed { get; set; }
+    public string color { get; set; }
+    public Car(string name; double speed)
+   public double TotalMile2
+    {
+        this.Name = name;
+        this.Speed = speed;
+    }
+
+}
